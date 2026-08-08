aec9ad4c6539082729b882219e5619e0ceec94d2
GavKesara <dissanayakegavesh@gmail.com>
2026-08-08 19:51:33 +0530

    Updated for JSON parsing

--- libcurl.h
+++ libcurl.h
@@ -1,29 +1,64 @@
 #include <stdio.h>
 #include <curl/curl.h>
+#include "json_functions.h"
 
-void libcurl_GET(CURL *curl,CURLcode res,FILE* fp,char* username){
+int libcurl_GET(char* username){
+
+    FILE *fp;
+    fp=fopen("curlGET.json","w");
+    if(fp==NULL){ 
+        printf("Error. File couldn't be created."); 
+        return -1;
+    }
+
+    CURL *curl;
+    CURLcode res;
+
+    struct MemoryChunk chunk;
+    chunk.memory=malloc(1);
+    chunk.size=0;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
     
     curl=curl_easy_init(); //initialize a curl easy handle
 
-    curl_easy_setopt(curl,CURLOPT_URL,"[https://api.github.com/users/](https://api.github.com/users/){username}/events");
+    char url[256];
+    snprintf(url,sizeof(url),"https://api.github.com/users/%s/events/public",username);
 
-    curl_easy_setopt(curl,CURLOPT_HTTPHEADER,"User-Agent: MyGitHubCLITool/1.0");
+    curl_easy_setopt(curl,CURLOPT_URL,url);
+
+    struct curl_slist *headers;
+    headers=curl_slist_append(headers,"User-Agent: MyGitHubCLITool/1.0");
+    curl_easy_setopt(curl,CURLOPT_HTTPHEADER,headers);
     
     curl_easy_setopt(curl,CURLOPT_FOLLOWLOCATION,1L); //Follow HTTP redirects if needed
 
     curl_easy_setopt(curl,CURLOPT_TIMEOUT,10L); //prevents CLI from waiting indefinitly.
 
-    curl_easy_setopt(curl,CURLOPT_WRITEDATA,fp); //writes outout data into fp
+    curl_easy_setopt(curl,CURLOPT_WRITEFUNCTION,WriteMemoryCallBack); //writes outout data into function
+
+    curl_easy_setopt(curl,CURLOPT_WRITEDATA,(void*)&chunk); //writes data into memorychunk which resizes depending on packets
 
     res=curl_easy_perform(curl); //perform the GET request. res will return the code
     
     if(res!=CURLE_OK){
-        fprintf(stderr,"curl_easy_perform() failed with error code : %s\n",curl_easy_strerr(res));
+        fprintf(stderr,"curl_easy_perform() failed with error code : %s\n",curl_easy_strerror(res));
     }else{
         long response_code;
         curl_easy_getinfo(curl,CURLINFO_RESPONSE_CODE,&response_code);
         printf("\nHTTP Response Code: %ld\n",response_code);
+        if(response_code==200){
+            cJSON *json = JSON_data(chunk,username);
+            cJSON_Delete(json);
+        }else if(response_code==404){
+            printf("\nError 404 Username not found.\n");
+            return -1;
+        }
     }
 
+    curl_slist_free_all(headers); //frees header
     curl_easy_cleanup(curl);
+
+    curl_global_cleanup();
+    fclose(fp);
 }
--- main.c
+++ main.c
@@ -4,16 +4,9 @@
 #include "client_functions.h"
 #include <curl/curl.h>
 #include "libcurl.h"
-
+#include <cjson/cJSON.h>
 
 int main(){
-    CURL *curl;
-    CURLcode res;
-    FILE *fp;
-
-    fp=fopen("curlGET.txt","w");
-
-    curl_global_init(CURL_GLOBAL_DEFAULT); //initialising global libcurl environment
 
     char User_name[40]; //Alloctes space for 19 charanters + '\0'
     printf("Github Activity : ");
@@ -23,9 +16,7 @@ int main(){
         Str_Mod_Space(User_name);
         User_name_check(User_name);
     }
+    libcurl_GET(User_name);
 
-
-    curl_global_cleanup();
-    fclose(fp);
     return 0;
 }
