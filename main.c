6779037c511dec35b70c7caaad0605c5b374ac0a
GavKesara <dissanayakegavesh@gmail.com>
2026-08-08 20:28:00 +0530

    Added command line argument feature

--- main.c
+++ main.c
@@ -8,16 +8,20 @@
 
 #define MAX_USER_CHAR 40
 
-int main(){
-
-    char User_name[MAX_USER_CHAR]; //Alloctes space for 19 charanters + '\0'
-    printf("Github Activity : ");
-    if(fgets(User_name,sizeof(User_name),stdin)!=NULL){
-        User_name[strcspn(User_name,"\r\n")]='\0';
-
-        Str_Mod_Space(User_name);
-        User_name_check(User_name,MAX_USER_CHAR);
+int main(int argc,char* argv[]){
+    if(argc!=2){
+        printf("Error! Irregular number of arguments.");
+        return(-1);
     }
+
+    char *User_name; //Alloctes space for 19 charanters + '\0'
+
+    User_name=argv[1];
+
+    User_name[strcspn(User_name,"\r\n")]='\0';
+
+    Str_Mod_Space(User_name);
+    User_name_check(User_name,MAX_USER_CHAR);
     libcurl_GET(User_name);
 
     return 0;
