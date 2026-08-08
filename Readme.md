2c3814e0c83d8b1f495f9c24581711844dad7d1c
GavKesara <dissanayakegavesh@gmail.com>
2026-08-08 17:15:59 +0530

    Added Client_functions header

--- Readme.md
+++ Readme.md
@@ -1,1 +1,2 @@
 Simple C Command line tool to retrive user activity of a User.
+Resources needed - [API 4 beginners](https://www.youtube.com/watch?v=WXsD0ZgxjRw)
---
+++ client_functions.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void Str_Mod_Space(char* name){
+    for(int i=0;name[i]!='\0';i++){
+        if(name[i]==' '){
+            name[i]='-';
+        }
+    }
+}
+
+int check(char C){
+    char lower_c=tolower(C);
+    if(lower_c>='a'&& lower_c<='z' || lower_c>='0'&&lower_c<='9'||lower_c=='-'){
+        return 0;
+    }
+    return 1;
+}
+
+void User_name_check(char* name){
+    for(int i=0;name[i]!='\0';i++){
+        if(name[0]=='-'){
+            printf("Error! Cannot contain - in beginning.");
+            break;
+        }
+
+        if(check(name[i])){
+            printf("Error! Check Username Rules");
+            break;
+        }
+    }
+}
